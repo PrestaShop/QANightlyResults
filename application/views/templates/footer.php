ea41815b49c012f7f2777fdadeb51da62704b618
Simon Garny <simon.garny@prestashop.com>
2019-08-01 12:45:52 +0200

    add GTAG

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,11 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-2753771-46"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-2753771-46');
+</script>
 </body>
 </html>
