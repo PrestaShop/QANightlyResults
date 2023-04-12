de7f25b29adb7d9b07a5d32b1db0f41e9b2b8fa2
Elodie Billiot <elodie.billiot@prestashop.com>
2023-04-12 09:19:24 +0200

    Feat: protect variable bucket

--- .github/scripts/manage-labels.sh
+++ .github/scripts/manage-labels.sh
@@ -24,15 +24,15 @@ fi
 echo ""
 echo "Checking if $BULLE is already being used..."
 
-if [ `gsutil ls gs://distribution-prestabulles |grep $DISTRIBUTION_APP |wc -l` -eq 1 ]
+if [ `gsutil ls gs://$BUCKET_PRESTABULLE |grep $DISTRIBUTION_APP |wc -l` -eq 1 ]
 then
   echo "Current application $DISTRIBUTION_APP has already been deployed, let's keep checking..."
   
-  if [ `gsutil ls gs://distribution-prestabulles/$DISTRIBUTION_APP/|grep $BULLE |wc -l` -eq 1 ]
+  if [ `gsutil ls gs://$BUCKET_PRESTABULLE/$DISTRIBUTION_APP/|grep $BULLE |wc -l` -eq 1 ]
   then
     echo "Current $BULLE prestabulle has already been defined as well, let's keep checking..."
 
-    gsutil -q cp gs://distribution-prestabulles/$DISTRIBUTION_APP/$BULLE .
+    gsutil -q cp gs://$BUCKET_PRESTABULLE/$DISTRIBUTION_APP/$BULLE .
 
     if [ ` grep ${GITHUB_HEAD_REF} $BULLE|wc -l` -eq 1 ]
     then
@@ -42,7 +42,7 @@ then
       echo "Please select ANOTHER available prestabulle :)"
       echo "FYI, here are the currently USED prestabulles:"
 
-      for i in `gsutil ls gs://distribution-prestabulles/$DISTRIBUTION_APP/ |awk -F'/' '{ print $NF}'`
+      for i in `gsutil ls gs://$BUCKET_PRESTABULLE/$DISTRIBUTION_APP/ |awk -F'/' '{ print $NF}'`
       do
         echo $i
       done 
@@ -57,13 +57,13 @@ then
 
   echo "Let's update $BULLE!"
   echo ${GITHUB_HEAD_REF} > $BULLE
-  gsutil -q cp $BULLE  gs://distribution-prestabulles/$DISTRIBUTION_APP/
+  gsutil -q cp $BULLE  gs://$BUCKET_PRESTABULLE/$DISTRIBUTION_APP/
 
 else
 
   echo "Let's create $BULLE!"
   echo ${GITHUB_HEAD_REF} > $BULLE
-  gsutil -q cp $BULLE  gs://distribution-prestabulles/$DISTRIBUTION_APP/
+  gsutil -q cp $BULLE  gs://$BUCKET_PRESTABULLE/$DISTRIBUTION_APP/
 
 fi
 
