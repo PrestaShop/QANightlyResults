71163efb2c97031c95adf774eccd16df272f2a97
Progi1984 <franck.lefevre@prestashop.com>
2024-01-09 14:56:26 +0100

    Added controller /

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testBadgeJson(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->isSuccessful());
+        $this->assertTrue($response->headers->has('content-type'));
+        $this->assertEquals('application/json', $response->headers->get('content-type'));
+
+        $content = $response->getContent();
+        $content = json_decode($content, true);
+        $this->assertEquals([], $content);
+    }
+}
